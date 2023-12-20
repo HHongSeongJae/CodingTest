17f7baa89be475f3f56d52b41caac1c966ca2a29
SeongJae Hong <hongsj2943@gmail.com>
2023-12-21 00:51:03 +0900

    update Level2_problem_23.12.21

--- Programmers/LV_2_할인 행사.py
+++ Programmers/LV_2_할인 행사.py
@@ -1,16 +1,33 @@
+#def solution(want, number, discount):
+#     answer = 0
+    
+#     for i in range(len(discount) - 9):
+#         print(i)
+#         check = []
+#         for j in want:
+#             num = discount[i:i+10].count(j)
+#             print(num)
+#             check.append(num)
+        
+#         if(check == number):
+#             answer = i + 1
+#             break
+
+#     return answer
+
 def solution(want, number, discount):
     answer = 0
     
+    new_list = []
+    for i in range(len(want)):
+        for j in range(number[i]):
+            new_list.append(want[i])
+    new_list.sort()
+    
     for i in range(len(discount) - 9):
-        print(i)
-        check = []
-        for j in want:
-            num = discount[i:i+10].count(j)
-            print(num)
-            check.append(num)
-        
-        if(check == number):
-            answer = i + 1
-            break
+        check = discount[i:i+10]
+        check.sort()
+        if(check == new_list):
+            answer += 1
 
     return answer
