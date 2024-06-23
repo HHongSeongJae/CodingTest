bd1f7be3ebfa7410b9eab389662ba4c9dea8ecb9
SeongJae Hong <hongsj2943@gmail.com>
2024-06-24 02:26:12 +0900

    add #25418__problem_24.06.24

---
+++ Backjoon/백준_25418번_정수 a를 k로 만들기.py
@@ -0,0 +1,21 @@
+## 단순 연산으로 풀이
+# A , K = map(int,input().split())
+
+# count = 0
+
+# while True:
+#     if K % 2 == 0 and A <= (K//2):
+#         K = K // 2
+#         count += 1
+#     else:
+#         K -= 1
+#         count += 1
+
+#     if A == K:
+#         print(count)
+#         break
+
+## BFS 적용 풀이
+
+
+
