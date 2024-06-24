6e2bf052a62fb10deeae4a2634ae994a157cbf40
SeongJae Hong <hongsj2943@gmail.com>
2024-06-25 01:51:42 +0900

    add #14248__problem_24.06.25

---
+++ Backjoon/백준_14248번_점프 점프.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+n = int(input())
+
+graph = [[] for _ in range(n+1)] # 0번 제외
+visited = [0] * (n+1)
+
+Ai = list(map(int,input().split()))
+
+for i in range(1,n+1):
+    graph[i].append(Ai[i-1])
+
+print(graph)
+
+s = int(input())
+
+def bfs(graph, start, visited):
+    queue = deque([start]) #시작노드 큐에 넣음
+    visited[start] = 1 # 방문처리
+
+    count = 1
+
+    while queue:
+        v = queue.popleft()
+
+        for i in(graph[v]):
+            if 0 < (v - i) <= n:    
+                if visited[v-i] == 0:
+                    queue.append(v-i)
+                    visited[v-i] = 1
+                    count += 1
+
+            if 0 < (v + i) <= n:
+                if visited[v+i] == 0:
+                    queue.append(v+i)
+                    visited[v+i] = 1
+                    count += 1
+
+    # print(visited)
+    return count
+
+print(bfs(graph,s,visited))
